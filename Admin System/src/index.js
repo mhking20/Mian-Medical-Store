270dfb99937e7962b8ec36a8eb65d4d6331e4703
mhking20 <www.taimoorking20@gmail.com>
2023-05-14 16:31:37 +0500

    hye

---
+++ Admin System/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import {Provider } from "react-redux";
+import reducer from "./reducer";
+import thunk from "redux-thunk";
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
