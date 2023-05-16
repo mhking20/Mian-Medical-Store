a7dfcc179aecbdb6b9e6c394ede32e9c2fceb229
mhking20 <www.taimoorking20@gmail.com>
2023-05-16 17:30:20 +0500

    updated

--- Admin_System/src/components/login.js
+++ Admin_System/src/components/login.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./Styles/login.css"
 
 function login() {
   return (
-    <div>login</div>
+    <div className='login-form-container'>
+        <form className='login-form'>
+            <h1 className='login-header'>Restricted Login Page</h1>
+            <input type="text" placeholder='Enter Username'></input>
+            <input type="password" placeholder="Enter Password"></input>
+            <button type='submit' className='login-btn'>Login</button>
+        </form>
+    </div>
   )
 }
 
