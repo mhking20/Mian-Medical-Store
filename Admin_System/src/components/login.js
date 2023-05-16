d86cc47b20ec0868b899ec2f988a86b3e79795f7
mhking20 <www.taimoorking20@gmail.com>
2023-05-16 17:47:30 +0500

    updated

--- Admin_System/src/components/login.js
+++ Admin_System/src/components/login.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import "./Styles/login.css"
+import {useNavigate} from "react-router-dom"
 
-function login() {
+function Login() {
+  const navigate = useNavigate()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        navigate("/home")
+    }
   return (
     <div className='login-form-container'>
-        <form className='login-form'>
-            <h1 className='login-header'>Restricted Login Page</h1>
+        <form className='login-form' onSubmit={(e) => handleSubmit(e)}>
+            <h3 className='login-header'>Restricted Login Page</h3>
             <input type="text" placeholder='Enter Username'></input>
             <input type="password" placeholder="Enter Password"></input>
             <button type='submit' className='login-btn'>Login</button>
@@ -14,4 +20,4 @@ function login() {
   )
 }
 
-export default login
+export default Login
