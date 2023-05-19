24adfcc66ec0aa8f6e059767be00f2e01ce3e2b7
mhking20 <www.taimoorking20@gmail.com>
2023-05-20 01:30:23 +0500

    hue

--- User_System/Client/src/components/home.js
+++ User_System/Client/src/components/home.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./styles/home.css";
-import panadol from "./images/panadol.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGreaterThan, faLessThan } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
@@ -33,6 +32,9 @@ const Footer = () => {
 
 const Content = () => {
   const [array, setarray] = useState([]);
+
+  const el = useRef();
+
   const Products = async () => {
     try {
       const products = await axios.get("http://localhost:5000");
@@ -43,9 +45,26 @@ const Content = () => {
       console.log(error);
     }
   };
+
   useEffect(() => {
     Products();
   }, []);
+
+  const handleClick = async (e) => {
+    const nextElement = e.target.nextSibling;
+    const previousElement = e.target.previousSibling;
+    if (nextElement) {
+      if (nextElement.textContent !== "0") {
+        let value = nextElement.textContent;
+        console.log(value--);
+        nextElement.textContent = value--;
+      }
+    } else if (previousElement) {
+      let value = previousElement.textContent;
+      console.log(value++);
+       previousElement.textContent = value++;
+    }
+  };
 
   return (
     <div className="home-container">
@@ -56,19 +75,31 @@ const Content = () => {
       <div className="home-items-container">
         {array.map((item) => {
           return (
-            <div className="home-item">
+            <div
+              className="home-item"
+              key={item._id}
+              data-set-id={item._id}
+            >
               <h2>{item.name}</h2>
               <hr />
               <img src={item.image} alt="img" height="100px" width="100px" />
               <div className="home-item-info">
                 <h4>Price = {item.price} $</h4>
                 <h4>InStock = {item.instock} items</h4>
-                <div className="home-icon-container">
-                  <FontAwesomeIcon icon={faLessThan} className="icon" />
-                  <h4 className="home-item-counter">0</h4>
-                  <FontAwesomeIcon icon={faGreaterThan} className="icon" />
+                <div className="home-icon-container" data-set-id={item._id}>
+                  <FontAwesomeIcon
+                    icon={faLessThan}
+                    className="icon"
+                    onClick={(e) => handleClick(e)}
+                  />
+                  <h4 className="home-item-counter" ref={el}>{0}</h4>
+                  <FontAwesomeIcon
+                    icon={faGreaterThan}
+                    className="icon"
+                    onClick={(e) => handleClick(e)}
+                  />
                 </div>
-                <button className="blue-button">Add To Cart</button>
+                <button className="blue-button">Buy Product</button>
               </div>
             </div>
           );
