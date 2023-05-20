0767533e000dba6196153c6075187f5e6005cf1b
mhking20 <113695912+mhking20@users.noreply.github.com>
2023-05-20 12:45:50 +0500

    Update index.js

--- User_System/Server/index.js
+++ User_System/Server/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const cors = require('cors');
 app.use(cors({
-  origin : ["https://mian-medical-store.netlify.app/"],
+  origin : ["https://mian-medical-store.netlify.app"],
   methods : ["GET" , "POST" , "DELETE" , "PATCH"]
 }))
 
